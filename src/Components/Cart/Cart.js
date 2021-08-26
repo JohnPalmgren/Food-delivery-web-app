387d4c0b9c49abb6b6cc8b064dd4953f71d1d270
John <johnmpalmgren@gmail.com>
2021-08-26 16:42:58 +0100

    state updated

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,21 +5,25 @@ const Cart = (props) => {
   const cartItem = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name:"Sushi", amount: "2", price: "2.99" }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onHideCart}>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        <button className={classes.button} onClick={props.onHideCart}>
+          Order
+        </button>
       </div>
     </Modal>
   );
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,17 +3,18 @@ import foodImage from "../../Assets/meals.jpg"
 import classes from "../Layout/Header.module.css"
 import HeaderCartButton from "./HeaderCartButton"
 
-const Header = () => {
-    return (
-      <React.Fragment>
-        <header className={classes.header}>
-          <h1>React Food</h1>
-          <HeaderCartButton />
-        </header>
-        <div className={classes["main-image"]}>
-          <img src={foodImage} alt="A table full of food" />
-        </div>
-      </React.Fragment>
+const Header = props => {
+
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>React Food</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={foodImage} alt="A table full of food" />
+      </div>
+    </React.Fragment>
     );
 }
 
